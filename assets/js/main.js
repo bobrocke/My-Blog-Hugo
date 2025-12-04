f0b8709c3c7fe3728b451a0c0c39f5fc359c7d11
Bob Rockefeller <bob@bobrockefeller.com>
2025-12-04 18:09:21 -0500

    Added my CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("Bob Rockefeller");
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function togglelinks() {
+  var x = document.getElementById("menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
